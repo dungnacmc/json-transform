ad55bfdcd45f3b4aef59fb4f55e9bd8166cc282d
Nguyen Anh Dung <nadung@cmc.com.vn>
2021-07-23 11:16:34 +0700

    Update comment

--- app/Http/Controllers/GitHubSearchController.php
+++ app/Http/Controllers/GitHubSearchController.php
@@ -4,14 +4,15 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\PaginationRequest;
 use App\Services\GitHubSearchService;
+use Illuminate\View\View;
 
 class GitHubSearchController extends Controller
 {
     /**
-     * return a view of pagination
+     * Return a view of pagination
      * @param PaginationRequest $request
      * @param GitHubSearchService $gitHubSearchService
-     * @return JsonResponse
+     * @return View
      */
     public function index(PaginationRequest $request, GitHubSearchService $gitHubSearchService)
     {
@@ -19,16 +20,18 @@ class GitHubSearchController extends Controller
         $itemPerPage    = $request->item_per_page ?? 10;
         $seekMethod     = $request->seek_method ?? '';
         $data           = $gitHubSearchService->getDataSearch($page, $itemPerPage, $seekMethod);
-        $dataRespone = [
+        $dataResponse = [
             'total_count'       => $data['total_count'],
             'items'             => $data['items'],
             'number_of_pages'   => $data['number_of_pages'][1] ?? $page,
             'current_page'      => $page,
             'item_per_page'     => $itemPerPage,
         ];
+
         if ($request->page || $request->item_per_page) {
-            return view('pagination.search', $dataRespone);
+            return view('pagination.search', $dataResponse);
         }
-        return view('pagination.index', $dataRespone);
+
+        return view('pagination.index', $dataResponse);
     }
 }
